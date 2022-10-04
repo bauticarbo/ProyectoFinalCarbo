076c1dcdf582dc64e1fc01df501081dcdba78c1d
bauticarbo <capocarbo@gmail.com>
2022-10-03 21:40:41 -0300

    Proyecto final

--- main.js
+++ main.js
@@ -28,14 +28,14 @@ function sumarAlCarrito(id) {
     .then((res) => res.json())
     .then((json) => {
       carrito.push(json[id]);
-      totalCarrito += json[id].precio;
+      totalCarrito = carrito.reduce((acc, el) => acc + el.precio, 0);
 
       carritoFinal.innerHTML = `<p>Los productos que colocó en el carrito son: </p>`;
       for (let i = 0; i < carrito.length; i++) {
         carritoFinal.innerHTML += `<div>- ${carrito[i].prod}  $${carrito[i].precio} <button class="btn-eliminar"  onclick="eliminarDelCarrito(${carrito[i].id}); renderCarrito()">Eliminar del carrito</button></div>`;
       }
 
-      carritoFinal.innerHTML += `<p> Sumando un total de $${totalCarrito}</p> <div><button class="btn-finalizar" id="btn-finalizar" onclick="">FINALIZAR COMPRA</button></div>`;
+      carritoFinal.innerHTML += `<p> Sumando un total de $${totalCarrito}</p> <div><button class="btn-finalizar" id="btn-finalizar" onclick="">FINALIZAR COMPRA</button></div><div><button class="btn-finalizar" id="btn-finalizar" onclick="vaciarCarrito()">VACIAR CARRITO</button></div>`;
 
       Toastify({
         text: 'Producto agregado!',
@@ -44,20 +44,23 @@ function sumarAlCarrito(id) {
     });
 }
 
-function eliminarDelCarrito(id) {
+function eliminarDelCarrito(index) {
   fetch('data.json')
     .then((res) => res.json())
     .then((json) => {
-      carritoAct = carrito.filter((el) => el.id != id);
-      carrito = carritoAct;
-      totalCarrito -= json[id].precio;
+      let o = carrito.findIndex((el) => {
+        return el.id === index;
+      });
 
+      carrito.splice(o, 1);
+
+      totalCarrito = carrito.reduce((acc, el) => acc + el.precio, 0);
       carritoFinal.innerHTML = `<p>Los productos que colocó en el carrito son: </p>`;
       for (let i = 0; i < carrito.length; i++) {
         carritoFinal.innerHTML += `<div>- ${carrito[i].prod}  $${carrito[i].precio} <button class="btn-eliminar"  onclick="eliminarDelCarrito(${carrito[i].id}); renderCarrito()">Eliminar del carrito</button></div>`;
       }
 
-      carritoFinal.innerHTML += `<p> Sumando un total de $${totalCarrito}</p> <div><button class="btn-finalizar" id="btn-finalizar" onclick="">FINALIZAR COMPRA</button></div>`;
+      carritoFinal.innerHTML += `<p> Sumando un total de $${totalCarrito}</p> <div><button class="btn-finalizar" id="btn-finalizar" onclick="">FINALIZAR COMPRA</button></div><div><button class="btn-finalizar" id="btn-finalizar" onclick="vaciarCarrito()">VACIAR CARRITO</button></div>`;
     });
   Toastify({
     text: 'Producto eliminado!',
@@ -70,15 +73,21 @@ function renderCarrito() {
   localStorage.setItem('carritoLS', carritoJSON);
 }
 
+function vaciarCarrito() {
+  carrito.splice(0);
+  totalCarrito = carrito.reduce((acc, el) => acc + el.precio, 0);
+}
+
 traerProductos();
 
 if (carritoEnLS) {
   carrito = carritoEnLS;
+  totalCarrito = carrito.reduce((acc, el) => acc + el.precio, 0);
   carritoFinal.innerHTML = `<p>Los productos que colocó en el carrito son: </p>`;
   for (let i = 0; i < carrito.length; i++) {
     carritoFinal.innerHTML += `<div>- ${carrito[i].prod}  $${carrito[i].precio} <button class="btn-eliminar"  onclick="eliminarDelCarrito(${carrito[i].id}); renderCarrito()">Eliminar del carrito</button></div>`;
   }
-  carritoFinal.innerHTML += `<p> Sumando un total de $${totalCarrito}</p> <div><button class="btn-finalizar" id="btn-finalizar" onclick="">FINALIZAR COMPRA</button></div>`;
+  carritoFinal.innerHTML += `<p> Sumando un total de $${totalCarrito}</p> <div><button class="btn-finalizar" id="btn-finalizar" onclick="">FINALIZAR COMPRA</button></div><div><button class="btn-finalizar" id="btn-finalizar" onclick="vaciarCarrito()">VACIAR CARRITO</button></div>`;
 } else {
   carrito = [];
   carritoFinal.innerHTML = '<b>Aún no tiene ningun producto en el carrito.</b>';
@@ -87,5 +96,3 @@ if (carritoEnLS) {
 carritoFinal.className = 'totalProductos';
 
 console.log(carrito);
-
-//-----------------------------------------------
